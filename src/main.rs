d936a8a8a2aa70e25d8e8ea5f4ef22ed0b116bdc
Aleksei Voronov <despawn@gmail.com>
2016-03-06 16:42:45 +0300

    Move the logic into a Game object
    Stuct? Class? What are these things called in Rust anyway?

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,54 @@ extern crate piston_window;
 use piston_window::*;
 
 
-fn main() {
-    let window: PistonWindow = WindowSettings::new(
-        "piston-tutorial",
-        [600, 600]
-    )
-    .exit_on_esc(true)
-    .build()
-    .unwrap();
+struct Game {
+    rotation: f64
+}
 
-    let mut rotation: f64 = 0.0;
-    for e in window {
-        match e.event {
-            Some(Event::Update(UpdateArgs { dt })) => {
-                rotation += 3.0 * dt;
-            }
-            _ => {
-            }
-        }
 
+impl Game {
+    fn new() -> Game {
+        Game { rotation : 0.0 }
+    }
+
+    fn update(&mut self, upd: UpdateArgs) {
+        self.rotation += 3.0 * upd.dt;
+    }
+
+    fn draw(&mut self, ren: RenderArgs, e: PistonWindow) {
         e.draw_2d(|c, g| {
             clear([0.0, 0.0, 0.0, 1.0], g);
-            let center = c.transform.trans(300.0, 300.0);
+            let center = c.transform.trans(
+                (ren.width / 2) as f64,
+                (ren.height / 2) as f64
+            );
             let square = rectangle::square(0.0, 0.0, 100.0);
             let red = [1.0, 0.0, 0.0, 1.0];
-            // translate the rectangle slightly so that it's centered;
-            // otherwise only the top left corner would be centered
-            rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), g);
+            rectangle(red, square, center.rot_rad(self.rotation).trans(-50.0, -50.0), g);
         });
+    }
+}
+
+
+fn main() {
+    let window: PistonWindow =
+        WindowSettings::new("piston-tutorial", [600, 600])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game::new();
+    for e in window {
+        match e.event {
+            Some(Event::Update(upd)) => {
+                game.update(upd);
+            }
+            Some(Event::Render(ren)) => {
+                game.draw(ren, e);
+            }
+            _ => {
+
+            }
+        }
     }
 }
